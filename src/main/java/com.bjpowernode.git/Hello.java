cd438989c4c6fbbc3196e8edacee6830f7f71e8f
zyy <zyy@gmail.com>
2022-01-16 23:01:01 +0800

    add bbbb

--- src/main/java/com.bjpowernode.git/Hello.java
+++ src/main/java/com.bjpowernode.git/Hello.java
@@ -19,5 +19,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("aaaa");
+        System.out.println("bbbb");
     }
 }
