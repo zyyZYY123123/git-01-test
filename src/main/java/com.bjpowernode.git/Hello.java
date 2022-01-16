2d212b76686909a6f3656f3d93b8073e426fe410
zyy <zyy@gmail.com>
2022-01-16 20:38:16 +0800

    init git-01-test

---
+++ src/main/java/com.bjpowernode.git/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * @项目名：git-01-test
+ * @创建人： zyy28
+ * @创建时间： 2022-01-16
+ * @公司： www.bjpowernode.com
+ * @描述：TODO
+ */
+package com.bjpowernode.git;
+
+/**
+ * <p>NAME: Hello</p>
+ * @author zyy28
+ * @date 2022-01-16 20:26:29
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
